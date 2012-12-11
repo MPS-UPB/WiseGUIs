6c5a9f9bde1014a3f984253e016ca65e3a0dcabf
RoxanaSevan <roxi_cristina_88@yahoo.com>
2012-12-11 23:07:14 +0200

    Parsarea parametrilor -- OK.Mai raman tipurile cu restrictiile lor.

--- T3 - Preprocessing GUI/APIGUIImplementation/src/mps/parser/implementation/Parser.java
+++ T3 - Preprocessing GUI/APIGUIImplementation/src/mps/parser/implementation/Parser.java
@@ -6,7 +6,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
-import javax.xml.namespace.NamespaceContext;
 
 import org.xml.sax.SAXException;
 
@@ -18,58 +17,115 @@ public class Parser {
 		XSOMParser parser = new XSOMParser();
 		parser.setErrorHandler(null);
 		parser.setEntityResolver(null);
+		XSType type = null;
+		XSContentType xsContentType = null;
+		List<String> list = new ArrayList<String>();
 		try {
-			parser.parse( new File(fileName));
+			parser.parse(new File(fileName));
 			XSSchemaSet sset = parser.getResult();
-			
-			Iterator<XSSchema> itr = sset.iterateSchema();
-			while( itr.hasNext() ) {
-			  XSSchema s = (XSSchema)itr.next();
 
-			  System.out.println("Target namespace: "+s.getTargetNamespace());
+			XSSchema s = (XSSchema) sset.getSchema(1);
 
-			  Iterator<XSElementDecl> jtr = s.iterateElementDecls();
-			
-			  while( jtr.hasNext() ) {
-			    XSElementDecl e = (XSElementDecl)jtr.next();
-			    System.out.println( e.getName());
-			    System.out.println("copii >"+e.asElementDecl());
-			   // XSElementDecl elem = e.getSubstAffiliation();
-			    XSType xsComplexType = e.getType();
-			   // XSContentType xscont = xsComplexType.getContentType();
-			    //XSParticle particle = xscont.asParticle();
-			    System.out.println("I? : "+xsComplexType);
-		     /*   while(i.hasNext()){
-		        	 XSAttributeDecl attributeDecl = i.next().getDecl(); 
-		             System.out.println("type: "+attributeDecl.getType());
-		             System.out.println("name:"+attributeDecl.getName());
-		        	}
-			  /* // System.out.println("\ncomplextype  -- "+e.getType().asComplexType());
-			    XSComplexType xsComplexType = e.getType().asComplexType();
-			    Iterator<? extends XSAttributeUse> i = xsComplexType.getAttributeUses().iterator();
-		        while(i.hasNext()){
-		        	 XSAttributeDecl attributeDecl = i.next().getDecl(); 
-		             System.out.println("type: "+attributeDecl.getType());
-		             System.out.println("name:"+attributeDecl.getName());
-		        	}
-			    */
-			  }
-			
-			  Iterator<XSComplexType> ztr = s.iterateComplexTypes();
-			    while( ztr.hasNext() ) {
-			    	System.out.println(((XSComplexType)ztr.next()).getName());
-			    }
-			  
+
+			// cauta definitii de tipuri complexe
+			Iterator<XSComplexType> ztr = s.iterateComplexTypes();
+			while (ztr.hasNext()) {
+
+				XSComplexType e = (XSComplexType) ztr.next();
+
+				System.out.println("Tip complex : " + e.getName());
+				
+				 
+				
 			}
+
+			// cauta tipuri simple
+			Iterator<XSSimpleType> str = s.iterateSimpleTypes();
+			while (str.hasNext()) {
+
+				XSSimpleType e = (XSSimpleType) str.next();
+
+				System.out.println("Tip simplu : " + e.getName());
+			
+			}
+
+			// cauta definitii de elemente - de fapt e unul singur - task
+			Iterator<XSElementDecl> jtr = s.iterateElementDecls();
+			while (jtr.hasNext()) {
+				XSElementDecl e = (XSElementDecl) jtr.next();
+				System.out.println(e.getName());
+				type = e.getType();
+				xsContentType = type.asComplexType().getContentType();
+				XSParticle xsParticle = xsContentType.asParticle();
+				getOptionalElements(list, xsParticle);
+			}
+
 		} catch (SAXException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
+	
+
+	private static void getOptionalElements(List <String> list, XSParticle xsParticle) {
+		if (xsParticle != null) {
+			XSTerm pterm = xsParticle.getTerm();
+			// daca am gasit un element inner
+			if (pterm.isElementDecl()) {
+
+				System.out.println("---------------------------------");
+				if (xsParticle.getMinOccurs() == 0) {
+					System.out.println("elem cu minOccurs = 0 : "
+							+ pterm.getSourceDocument().getTargetNamespace()
+							+ ":" + pterm.asElementDecl().getName());
+					/*list.add(pterm.getSourceDocument().getTargetNamespace()
+							+ ":" + pterm.asElementDecl().getName());*/
+				}
+
+				
+				System.out.println("Element Name : "
+						+ pterm.asElementDecl().getName());
+				System.out.println("Element Type : "
+						+ pterm.asElementDecl().getType().getName());
+				System.out.println("---------------------------------");
+
+				XSComplexType xsComplexType = (pterm.asElementDecl()).getType()
+						.asComplexType();
+
+				
+				//nu cred ca trebuie sa testam pentru enumeration si alte minuni
+				//daca nu apar in exemplele date
+				/*
+				if (xsComplexType != null
+						&& !(pterm.asElementDecl().getType()).toString()
+								.contains("Enumeration")) {
+
+					XSContentType xsContentType = xsComplexType
+							.getContentType();
+
+					XSParticle xsParticleInside = xsContentType.asParticle();
+					getOptionalElements(list, xsParticleInside);
+				}
+*/
+				
+				
+			}
+			
+			else if (pterm.isModelGroup()) {
+
+				XSModelGroup xsModelGroup2 = pterm.asModelGroup();
+				XSParticle[] xsParticleArray = xsModelGroup2.getChildren();
+				for (XSParticle xsParticleTemp : xsParticleArray) {
+					getOptionalElements(list, xsParticleTemp);
+				}
+			}
+		}
+	}
+	
+	
+	
 	
 	public static  List<Operation> getExecTypes(){
 	
